1f74cd5dcd2890a0240a088f351d4bb35ea1d345
Shingwa Six <shingwasix@gmail.com>
2015-08-04 11:09:59 +0800

    修改测试用例

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -7,6 +7,7 @@
 //
 
 @import XCTest;
+#import <MobShareSDK/ShareSDK/ShareSDK.h>
 
 @interface Tests : XCTestCase
 
@@ -26,9 +27,37 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testRegister
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    [ShareSDK registerApp:@"1234567890abcdefghijk" useAppTrusteeship:YES];
+}
+
+- (void)testSimpleShareView
+{
+    id<ISSContent> publishContent = [ShareSDK content:@"MobShareSDK"
+                                       defaultContent:@"MobShareSDK"
+                                                image:nil
+                                                title:@"MobShareSDK"
+                                                  url:@"https://github.com/shingwasix/MobShareSDK"
+                                          description:@"MobShareSDK"
+                                            mediaType:SSPublishContentMediaTypeText];
+    
+    [ShareSDK showShareViewWithType:ShareTypeSinaWeibo
+                          container:nil
+                            content:publishContent
+                      statusBarTips:YES
+                        authOptions:nil
+                       shareOptions:[ShareSDK simpleShareOptionsWithTitle:@"分享" shareViewDelegate:nil]
+                             result:^(ShareType type, SSResponseState state, id<ISSPlatformShareInfo> statusInfo, id<ICMErrorInfo> error, BOOL end) {
+                                 if (state == SSPublishContentStateSuccess)
+                                 {
+                                     NSLog(NSLocalizedString(@"TEXT_SHARE_SUC", @"发表成功"));
+                                 }
+                                 else if (state == SSPublishContentStateFail)
+                                 {
+                                     NSLog(NSLocalizedString(@"TEXT_SHARE_FAI", @"发布失败!error code == %d, error code == %@"), [error errorCode], [error errorDescription]);
+                                 }
+                             }];
 }
 
 @end
